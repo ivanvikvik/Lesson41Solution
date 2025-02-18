ef7bad5eb0a555eb844f9e020b8332aa37b223db
ivanvikvik <ivanvikvik@gmail.com>
2025-02-18 16:58:09 +0300

    Add project files.

---
+++ Task01Project/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 10
+#define MAX_MARK 10
+
+int main() {
+	int marks[SIZE];
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		marks[i] = rand() % (MAX_MARK + 1);
+	}
+
+	cout << "Student's marks: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << marks[i] << " ";
+	}
+
+	double avg = 0;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		avg += marks[i];
+	}
+
+	avg = avg / SIZE;
+
+	cout << "\nAverage mark is " << avg << endl;
+
+	return 0;
+}
