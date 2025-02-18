af979f6882fb1ddfa5d324151f570ace89ff27b4
ivanvikvik <ivanvikvik@gmail.com>
2025-02-18 17:26:50 +0300

    Add implementation for three classes: A, B and C

---
+++ Task01Project/logic.h
@@ -0,0 +1,4 @@
+#include "util.h"
+
+double calc_avg_mark(int* marks, int size);
+string find_best_class(double a, double b, double c);
--- Task01Project/main.cpp
+++ Task01Project/main.cpp
@@ -1,33 +1,31 @@
-#include <iostream>
-using namespace std;
+#include "logic.h"
 
 #define SIZE 10
 #define MAX_MARK 10
+#define MIN_MARK 0
 
 int main() {
-	int marks[SIZE];
+	int marksA[SIZE];
+	int marksB[SIZE];
+	int marksC[SIZE];
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		marks[i] = rand() % (MAX_MARK + 1);
-	}
+	init(marksA, SIZE, MIN_MARK, MAX_MARK);
+	init(marksB, SIZE, MIN_MARK, MAX_MARK);
+	init(marksC, SIZE, MIN_MARK, MAX_MARK);
 
-	cout << "Student's marks: ";
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << marks[i] << " ";
-	}
+	cout << "Student's marks of class A: " << convert(marksA, SIZE) << endl;
+	cout << "Student's marks of class B: " << convert(marksB, SIZE) << endl;
+	cout << "Student's marks of class C: " << convert(marksC, SIZE) << endl;
+	
+	double avgA = calc_avg_mark(marksA, SIZE);
+	double avgB = calc_avg_mark(marksB, SIZE);
+	double avgC = calc_avg_mark(marksC, SIZE);
+	   
+	cout << "\nAverage mark of class A is " << avgA << endl;
+	cout << "Average mark of class B is " << avgB << endl;
+	cout << "Average mark of class C is " << avgC << endl;
 
-	double avg = 0;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		avg += marks[i];
-	}
-
-	avg = avg / SIZE;
-
-	cout << "\nAverage mark is " << avg << endl;
+	cout << "\nBest class is " << find_best_class(avgA, avgB, avgC) << endl;
 
 	return 0;
 }
